2d74a28db7efb61b5d32864beabf4eeef8739b7d
Yursksf1 <Yurs.Ksf1@gmail.com>
2025-01-28 19:51:40 -0500

    Create aritmetica.py

---
+++ clase_1/aritmetica.py
@@ -0,0 +1,34 @@
+# SUMA  (+)
+print(3+4)
+print(4+3)
+
+# RESTA (-)
+print(3-4)
+print(4-3)
+
+# Multiplicacion (*)
+print(3*4)
+print(4*3)
+
+# Division (/) 
+print(12/3)
+print(11/3)
+
+# Potencia (**)
+print(2**5)
+print(5**2)
+
+# Raiz n (r**(1/n))
+print(25**(1/2))
+print(1024**(1/10))
+
+# Parte entera (//)
+print(12//3)
+print(13//3)
+print(14//3)
+print(15//3)
+
+# mod o Residuo (%)
+print(13 % 3)
+print(14 % 3)
+print(15 % 3)
