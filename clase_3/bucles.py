0f9c85b9cfb1311ce962a2d4867ac11bb142c9f0
Yursksf1 <Yurs.Ksf1@gmail.com>
2025-02-06 20:43:41 -0500

    Create bucles.py

---
+++ clase_3/bucles.py
@@ -0,0 +1,16 @@
+# listas
+frutas = ["manzana", "mango", "fresa", "coco", "kiwi"]
+for fruta in frutas:
+    print(fruta)
+
+# tuplas 
+verduras = ("coliflor", "espinaca", "brocoli")
+for verdura in verduras:
+    print(verdura)
+
+# rangos 
+for numero in range(10):
+    print(numero)
+
+for elemento in range(25, 30):
+    print(elemento)
