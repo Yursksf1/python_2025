ae4e7487836a02a5d9a1280334df0d5cb900d40d
YursKsf1 <yurs.ksf@gmail.com>
2025-02-11 19:55:13 -0500

    more while

--- clase_4/bucle_while.py
+++ clase_4/bucle_while.py
@@ -5,3 +5,19 @@ contador = 0
 while contador<10:
     print(contador)
     contador = contador+1
+
+bandera = True
+contador = 0
+while bandera:
+    print(contador)
+    contador = contador+1
+    if contador>10:
+        bandera = False
+
+contador = 0
+while True:
+    print(contador)
+    contador = contador+1
+    if contador>10:
+        break
+    
