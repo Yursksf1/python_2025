bbe8872295d9d2118fe1d1fd2a83fc07bb74d2f0
YursKsf1 <yurs.ksf@gmail.com>
2025-02-11 19:12:12 -0500

    solucion tablas de verdada con bucles

--- clase_4/solucion_tablas_verdad.py
+++ clase_4/solucion_tablas_verdad.py
@@ -1,3 +1,9 @@
-p = True
-print(p)
-print(not p)
+p_values = [0, 0, 1, 1]
+q_values = [0, 1, 0, 1]
+
+# (p or not p) and q
+for i in range(len(p_values)):
+    p = p_values[i]
+    q = q_values[i]
+    respuesta = p and not q
+    print(p, q, respuesta)
