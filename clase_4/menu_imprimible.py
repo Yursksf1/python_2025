c66d287f7c17dd812e348ae1fa267f97b04d7c7d
YursKsf1 <yurs.ksf@gmail.com>
2025-02-13 20:56:43 -0500

    define functions

--- clase_4/menu_imprimible.py
+++ clase_4/menu_imprimible.py
@@ -1,14 +1,18 @@
-while True:
-    # Imprimir el menú
+def ver_menu():
     print("Menú:")
     print("1. Opción 1")
     print("2. Opción 2")
     print("3. Opción 3")
     print("4. Salir")
+
+while True:
+    # Imprimir el menú
+    ver_menu()
     # Solicitar al usuario que ingrese una opción
     opcion = input("Ingrese el número de opción que desea: ")
     # Procesar la opción seleccionada
     if opcion == "1":
+        
         print("Ha seleccionado la Opción 1.")
     elif opcion == "2":
         print("Ha seleccionado la Opción 2.")
