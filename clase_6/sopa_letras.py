e6166936798ed7cd70c2153e90bf17ee1d2bcabd
YursKsf1 <yurs.ksf@gmail.com>
2025-02-18 21:29:57 -0500

    sopa de letras super dificil

--- clase_6/sopa_letras.py
+++ clase_6/sopa_letras.py
@@ -2,23 +2,41 @@ import random
 def create_matrix(elemento, palabra, filas, columnas):
     opciones = ["p", "l", "a", "b", "r"]
     matriz = [[(random.choice(opciones)) for _ in range(columnas)] for _ in range(filas)]
+    opcion = random.choice(["1", "2", "3","4", "5", "6"])
+
     r_col = random.randint(0,columnas-1-len(palabra))
     r_fil = random.randint(0,filas-1-len(palabra))
-
+    if opcion in ["4", "5", "6"]:
+        r_col = random.randint(0+len(palabra),columnas-1)
+        r_fil = random.randint(0+len(palabra),filas-1)
     contador_col = r_col
     contador_fil = r_fil
     for p in palabra:
-        if False: # case horizontal
+        if opcion == "1": # case horizontal
             matriz[r_fil][contador_col] = p
             contador_col=contador_col+1
-        elif True: # case vertical
+            
+        elif opcion == "2": # case vertical
             matriz[contador_fil][r_col] = p
-            contador_fil = contador_fil+1 # your code is here
+            contador_fil = contador_fil+1 
         
-        elif False: # case diagonal
+        elif opcion == "3": # case diagonal
             matriz[contador_fil][contador_col] = p
             contador_col=contador_col+1
             contador_fil=contador_fil+1
+            
+        elif opcion == "4": # case horizontal atras
+            matriz[r_fil][contador_col] = p
+            contador_col=contador_col-1
+
+        elif opcion == "5": # case vertical arriba
+            matriz[contador_fil][r_col] = p
+            contador_fil = contador_fil-1 
+            
+        elif opcion == "6": # case vertical arriba
+            matriz[contador_fil][contador_col] = p
+            contador_fil = contador_fil-1 
+            contador_col=contador_col-1
         
     return matriz
 
